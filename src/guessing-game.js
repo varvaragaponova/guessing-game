f6b06e0bd2a8d4d18478b6a33235d3bea96b8a47
Varvara Gaponova <gaponova.varvara21091995@gmail.com>
2023-02-20 19:52:32 +0400

    feat: added function

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,26 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        this.arr = [];
+        this.mediumNumber;
+    }
 
     setRange(min, max) {
-
+        for(let i = min; i <= max; i++) {
+            this.arr.push(i);
+        }
     }
 
     guess() {
-
+        this.mediumNumber = this.arr[Math.floor(this.arr.length / 2)];
+        return this.mediumNumber;
     }
 
     lower() {
-
+        this.arr = this.arr.filter(item => item <= this.mediumNumber);
     }
 
     greater() {
-
+        this.arr = this.arr.filter(item => item >= this.mediumNumber);
     }
 }
 
